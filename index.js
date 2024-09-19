2d8c0d4bc939ba337f0674d8655861e1d86329f8
Rodrigo Stoffel <rodrigo.stoffel@gmail.com>
2024-09-18 21:37:21 -0300

    Formulário funcionando

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ botaoFecharEdit.addEventListener("click", function () {
   let popup = document.querySelector(".popup");
   popup.classList.remove("popup_opened");
 });
+
+let formElement = document.querySelector("form");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let nameInput = document.querySelector("input[name='name']");
+  let jobInput = document.querySelector("input[name='job']");
+
+  let nameValue = nameInput.value;
+  let jobValue = jobInput.value;
+
+  let profileNameElement = document.querySelector(".profile__name");
+  let profileJobElement = document.querySelector(".profile__job");
+
+  profileNameElement.textContent = nameValue;
+  profileJobElement.textContent = jobValue;
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
+console.log();
